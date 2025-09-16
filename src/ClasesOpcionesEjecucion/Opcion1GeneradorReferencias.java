7f15e90794fc8f2a137ee4060fb2386f4e606dd8
Emmanuel Blanco <ef.blanco@uniandes.edu.co>
2025-09-16 09:50:01 -0500

    Primeros cambios en opcion 1

--- src/ClasesOpcionesEjecucion/Opcion1GeneradorReferencias.java
+++ src/ClasesOpcionesEjecucion/Opcion1GeneradorReferencias.java
@@ -1,5 +1,8 @@
 package ClasesOpcionesEjecucion;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Opcion1GeneradorReferencias 
 {
     /*
@@ -8,4 +11,28 @@ public class Opcion1GeneradorReferencias
      * de memoria
      */
 
+     public void generarReferencia(int tamanioPag, int filas, int columnas)
+     {
+        int nr = filas*columnas*3;
+        int tamanioMatrices = nr*4;
+        int numPag = (int) Math.ceilDiv(tamanioMatrices, nr);
+
+        try{
+            File proc = new File("procX.txt");
+            if(proc.createNewFile())
+            {
+                System.out.println("Referencia al proceso X creada como");
+            }
+            else{
+                System.out.println("Ya existe un archivo con ese nombre");
+            }
+        }
+        catch(IOException e){
+            System.out.println("Hubo un error");
+            e.printStackTrace();
+        }
+        
+
+
+     }
 }
